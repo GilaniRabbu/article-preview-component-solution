d66857845d6a7317ebb9735e2f94864972c01fe4
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-01-01 23:52:01 +0600

    Initial commit

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const btnCardShare = document.getElementById('btn__card--share');
-const cardShare = document.getElementById('card__share');
+var iconsShareBlock = document.querySelector('.icon__share'),
+  networks = document.querySelector('.networks'),
+  iShare = document.querySelector('.i-share');
 
-btnCardShare.addEventListener('click', () => {
-  cardShare.classList.toggle('fade__in');
-});
+iconsShareBlock.addEventListener('click', function () {
+  if (networks.style.visibility === '' || networks.style.visibility === 'hidden') {
+    networks.style.visibility = 'visible';
+    this.style.background = 'var(--clr-neutral-300)';
+    iShare.style.fill = 'var(--clr-neutral-100)';
+    return;
+  }
 
-cardShare.addEventListener('mouseleave', () => {
-  cardShare.classList.remove('fade__in');
+  networks.style.visibility = 'hidden';
+  this.style.background = '';
+  iShare.style.fill = '';
 });
